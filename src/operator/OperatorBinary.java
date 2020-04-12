8db69fce18cd1ab5c936628e3b773c714227998a
AhmadYasir0171 <ahmadyasir473@gmail.com>
2020-04-12 09:38:50 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Ahmad Yasir";
+    int jumlahSks = 24;
+    double indeksPresentasiKumulatif = 3.89;
+}
