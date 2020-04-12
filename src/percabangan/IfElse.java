4619e9d84e18254be0509953383e9fc67f1a40d3
AhmadYasir0171 <ahmadyasir473@gmail.com>
2020-04-12 10:45:42 +0800

    If dengan conditional1 true, conditional2 false sehingga statement1 jalan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,16 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args){
-        double totalBelanja = 30000;
-        double uangDiDompet = 60000;
+        double totalBelanja = 60000;
+        double uangDiDompet = 30000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang Tidak cukup, kurangi jajan anda");
+        }else if (uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, angsul : "+angsul);
         }else{
-            System.out.println("Uang cukup, Berelaan lah");
+            System.out.println("Uang pas..Alhamdulilah..");
         }
     }
 }
