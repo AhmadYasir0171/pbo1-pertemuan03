0293954fc3c361671fad8a545744346b3b4a8117
AhmadYasir0171 <ahmadyasir473@gmail.com>
2020-04-12 10:35:10 +0800

    If dengan conditional false

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,13 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args){
-        double totalBelanja = 60000;
-        double uangDiDompet = 30000;
+        double totalBelanja = 30000;
+        double uangDiDompet = 60000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang Tidak cukup, kurangi jajan anda");
         }else{
-            System.out.println("Uang cukup, Selamat menikmati");
+            System.out.println("Uang cukup, Berelaan lah");
         }
     }
 }
