e2b284cfc28d1d3c8d991b593feafa75ddf1e523
AhmadYasir0171 <ahmadyasir473@gmail.com>
2020-04-12 09:44:56 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,4 +9,13 @@ public class OperatorBinary {
     String nama = "Ahmad Yasir";
     int jumlahSks = 24;
     double indeksPresentasiKumulatif = 3.89;
+
+    public static void main(String[] args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
